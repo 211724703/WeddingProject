4ea6670b8262bda0053d634f6f705aa56aa1c0e7
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-16 19:03:27 +0300

    Update SadottypeofworkerCollection.cs

--- webApi/‏‏תיקיה חדשה/Project/WeddingProject/Controllers/SadottypeofworkerCollection.cs
+++ webApi/‏‏תיקיה חדשה/Project/WeddingProject/Controllers/SadottypeofworkerCollection.cs
@@ -13,10 +13,10 @@ namespace WeddingProject.Controllers
     public class SadottypeofworkerCollection : ControllerBase
     {
         ISadottypeofworkerService iSadottypeoS;
-        public SadottypeofworkerCollection(ISadottypeofworkerService iSadottypeoS) //זה הזרקה לסרויס
+        public SadottypeofworkerCollection(ISadottypeofworkerService iSadottypeoS) 
         {
             this.iSadottypeoS = iSadottypeoS;
-            //פה יכול  להיות לך הרבה הזרקות להרבה סרויסיםץ אל תשכחי להוסיף אותם בפעולה הבונה
+            
         }
 
         // GET: api/<controller>
