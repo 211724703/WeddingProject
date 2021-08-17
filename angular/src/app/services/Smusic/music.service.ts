c32d057afcbb212c28fcfaf5e521145760881424
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 18:28:56 +0300

    Update music.service.ts

--- angular/src/app/services/Smusic/music.service.ts
+++ angular/src/app/services/Smusic/music.service.ts
@@ -11,7 +11,7 @@ export class MusicService {
   urlservice='http://localhost:57438/api/';
   public work:workers;
   private _work1:workers;
-  constructor(private http :HttpClient) { ///מקבל את הhttp והוא מעביר את הקריאות שרת
+  constructor(private http :HttpClient) { 
   }
   
 
@@ -22,38 +22,23 @@ export class MusicService {
   set worker(worker:workers){
      this._work1 = worker;
   }
-  //החזרת פרטי בעל מיקצוע + מחיר
-  //   getOne(id:number):Observable<any[]>//החזרת שורה בטבלה 
-  //  {
-  //   debugger;
-  //     return  this.http.get<any[]>(this.urlservice+"Workers/GetDetailsWorkerById/"+id);
-      
-  //  }
-  
-   // ינפורמציה החזרת פרטי בעל מיקצוע + מחיר
-  //  getOnee(id1:number,id2:number):Observable<any[]>//החזרת שורה בטבלה 
-  //  {
-  //   debugger;
-  //     return  this.http.get<any[]>(this.urlservice+"Workers/GetinformWorker/"+id1+"/"+id2);
-      
-  //  }
-
-  getall():Observable<any> //החזרת רשימה שלטבלה   ם
+ 
+  getall():Observable<any> 
   {
    debugger;
-   return  this.http.get(this.urlservice+ "Workers") ;//הכנסת הנתיב מווב לטבלת בעלי המצוע
+   return  this.http.get(this.urlservice+ "Workers") ;
    
   }
 
-  //החזרת פרטי בעל מיקצוע + מחיר
-    getOne(id:number):Observable<any[]>//החזרת שורה בטבלה 
+  
+    getOne(id:number):Observable<any[]>
    {
     debugger;
       return  this.http.get<any[]>(this.urlservice+"Workers/GetDetailsWorkerByType/"+id);
       
    }
 
-   getMoreinfo(id:number):Observable<any[]>//החזרת שורה בטבלה 
+   getMoreinfo(id:number):Observable<any[]>
    {
     
       return  this.http.get<any[]>(this.urlservice+"Workers/GetDetailsWorkerById/"+id);
