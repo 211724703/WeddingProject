3a5000c243423007dc196d02e1259bec1d8b1c5a
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 17:56:46 +0300

    Update log-in.component.ts

--- angular/src/app/components/log-in/log-in.component.ts
+++ angular/src/app/components/log-in/log-in.component.ts
@@ -25,21 +25,20 @@ export class LogInComponent implements OnInit {
 
   }
   
-  //פתיחת קומפוננטה של משתמשים חדשים
+  
   newuser(){
    debugger;
   this.router.navigate(["new-user", 0]); 
   }
 
-  //פתיחת קומפוננטה של אזור אישי
+  
   personall(){
    debugger;
    this.router.navigate(["new-user", this.user.userId]); 
    // this.router.navigateByUrl("personal"); debugger;
     }
 
-  //פונקצית שליפה שם משתמש וסיסמא
-  //אני אמורה לקבל את שניהם או רק את הסיסמא
+ 
   connect()   { 
         this.serv.findCustomer(this.user.userId,this.user.username).subscribe(data=>{
         this.serv.setCustomer(data);
@@ -53,48 +52,6 @@ export class LogInComponent implements OnInit {
       });
    
 
-      // this.serv.findCustomer(this.user.userId,this.user.username);debugger;
-      // this.serv.cust.subscribe(data=>{
-      //   debugger;
-      //  // this.serv.cust=data;
-      //   this.customer1=data
-      //   if(this.customer1===undefined)
-      //   alert("try again");
-      //   else
-      //   alert(this.customer1.namchatan);
-      
-      // });//הלקוח הזה יכיל את הפרטים שקיבלנו
-  
      
-    }
-  }
-
-
-//       this.user.findCustomer(this.userId,this.username)
-//       .subscribe( data => {debugger;
-//                  if (data) {
-//                   //לומר לו להכניס את הפרטים
-//                      new alert("succses");
-//                  }
-//                  else {
-//                     new alert("try agian");
-//                  }
-//              },
-//              errors => {
-//                  console.log("request failed");
-//              }
-// ); 
-// ;
-// addcustomer(){debugger;
-//   //customer.push()
-//   if(this.user.username==this.paaswordc)
-//   {
-//    this.serv.submit(this.user).subscribe();   
-//   }
-//    else
-//    alert("הסיסמה שגויה נסה שוב ");debugger;
-//   }
-
-
 
   
