219b71349b2f753c172725127e1ae0313a1e6b7e
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 17:25:06 +0300

    Update WorkersregistrationServise.cs

--- webApi/‏‏תיקיה חדשה/Project/Service/Service/WorkersregistrationServise.cs
+++ webApi/‏‏תיקיה חדשה/Project/Service/Service/WorkersregistrationServise.cs
@@ -13,26 +13,26 @@ namespace Service
     {
         IWorkersregistrationRepository iWorkersreR;
 
-        public WorkersregistrationServise(IWorkersregistrationRepository iWorkersreR)//גישה לרפוזיטורי
+        public WorkersregistrationServise(IWorkersregistrationRepository iWorkersreR)
         {
             this.iWorkersreR = iWorkersreR;
         }
         public void AddWorkersregistration(CWorkersregistration Worker)
         {
-            //CBlocks cBlocks = new CBlocks() { IdBlock=block.IdBlock,NameBlocks=block.NameBlocks};
+            
             Workersregistration workersregistration = new Workersregistration()
             {
 
-                WorkersregistrationId = Worker.WorkersregistrationId,//סיסמה
-                Dateworkersregistration = Worker.Dateworkersregistration,//שם משתמש
+                WorkersregistrationId = Worker.WorkersregistrationId,
+                Dateworkersregistration = Worker.Dateworkersregistration,
                 Discreptionworkersregistration = Worker.Discreptionworkersregistration,
                 Mailworker = Worker.Mailworker,
                 Statusid = Worker.Statusid,
                 NameworkersregistrationId= Worker.NameworkersregistrationId,
             };
 
-            iWorkersreR.AddWorkersregistration(workersregistration);//זהו , זה ההוספה וככה את עושה גם למחיקה והעדכון אשלח לך דוגמא מפורטת
-        }                            //עכשיו נעשה את הקונטרולר
+            iWorkersreR.AddWorkersregistration(workersregistration);
+            }
 
 
         public void DeleteWorkersregistration(CWorkersregistration Worker)
@@ -40,8 +40,8 @@ namespace Service
             CWorkersregistration cworkersregistration = new CWorkersregistration()
             {
 
-                WorkersregistrationId = Worker.WorkersregistrationId,//סיסמה
-                Dateworkersregistration = Worker.Dateworkersregistration,//שם משתמש
+                WorkersregistrationId = Worker.WorkersregistrationId,
+                Dateworkersregistration = Worker.Dateworkersregistration,
                 Discreptionworkersregistration = Worker.Discreptionworkersregistration,
                 Mailworker = Worker.Mailworker,
                 Statusid = Worker.Statusid,
@@ -52,8 +52,8 @@ namespace Service
             Workersregistration workersregistration = new Workersregistration()
             {
 
-                WorkersregistrationId = Worker.WorkersregistrationId,//סיסמה
-                Dateworkersregistration = Worker.Dateworkersregistration,//שם משתמש
+                WorkersregistrationId = Worker.WorkersregistrationId,
+                Dateworkersregistration = Worker.Dateworkersregistration,
                 Discreptionworkersregistration = Worker.Discreptionworkersregistration,
                 Mailworker = Worker.Mailworker,
                 Statusid = Worker.Statusid,
@@ -92,7 +92,7 @@ namespace Service
 
 
 
-        public List<CWorkersregistration> GetAllWorkersregistration()//מחזיר רשימה 
+        public List<CWorkersregistration> GetAllWorkersregistration()
         {
 
             List<CWorkersregistration> workersregistration = (from tmp in iWorkersreR.GetAllWorkersregistration()
@@ -108,21 +108,7 @@ namespace Service
 
             return workersregistration;
         }
-        //public void Sendemail(string to, string from, string path, string treename)
-        //{
-        //    MailMessage mail = new MailMessage();
-        //    mail.To.Add(new MailAddress(to));
-        //    mail.From = new MailAddress(from);
-        //    mail.Subject = "בהמשך לפנייתך ל" + treename;
-        //    Attachment attachment = new Attachment(path);
-        //    mail.Attachments.Add(attachment);
-        //    try
-        //    {
-        //        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-        //        smtp.UseDefaultCredentials = false;
-        //        smtp.Credentials = new System.Net.NetworkCredential("smart.nevigation");
-        //    }
-        //}
+     
 
     }
 }
