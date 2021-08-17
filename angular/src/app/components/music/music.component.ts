40c9b4d6ae207b8529b81717c628ef9e85bcd333
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 17:59:53 +0300

    Update music.component.ts

--- angular/src/app/components/music/music.component.ts
+++ angular/src/app/components/music/music.component.ts
@@ -17,17 +17,17 @@ import { workers } from '../../Classes/worker';
 export class MusicComponent implements OnInit {
   [x: string]: any;
   
-  Workers:any[]=[];//הגדרת מערך
+  Workers:any[]=[];
   Workers1:any[]=[];
-  id:number;//הגדרת ינט רגיל
-  worker:any= {};//מופע בודד
+  id:number;
+  worker:any= {};
   public work:workers;
   form;
   category:string;
   zaner:string;
   aria:string;
   // user:any
-  constructor(private router:Router ,private serve:MusicService) { //מזמנים את הסרוויס)
+  constructor(private router:Router ,private serve:MusicService) { 
    
   }
 
@@ -37,26 +37,26 @@ export class MusicComponent implements OnInit {
   }
   ngOnInit() {
   
-    // זימון הפונקציה המחזירה את רשימת בעלי המקצוע
+    
      this.serve.getOne(2).subscribe(data =>{
        debugger;
        console.log('רשימה של מוזיקה',data)     
-       this.Workers=data;debugger; //החזרה בצורת רשימה 
+       this.Workers=data;debugger;  
        console.log(this.Workers);
-       this.Workers1=this.Workers;debugger;  //החזרה בצורת טבלה 
+       this.Workers1=this.Workers;debugger;   
       });
     }
 
-    //פונקציה בשביל הצ'ק בוקס
+   
 changed(evt) {
   debugger;
 if(evt.checked)
 {debugger;
-  this.category=evt.source.name; //מה שנלחץ נכנס לשדה הקטגוריה
+  this.category=evt.source.name; 
   if(this.category=='כל האזורים')
       return this.Workers1=this.Workers;
   else{
-    this.Workers1=this.Workers.filter(item=>{debugger;return item.category==this.category}  );//בדיקה שהאלמט שנבחר  זהה לשדה קטגוריה שיש לזמר
+    this.Workers1=this.Workers.filter(item=>{debugger;return item.category==this.category}  );
   }
 }
 else {
@@ -65,7 +65,7 @@ else {
 }
 
 
-// פתיחת קומפוננטה של מידע נוסף 
+ 
 moreinff(todo){
  this.serve.worker= todo; 
  this.router.navigate(["moreinf",todo.workerid]); debugger;
@@ -78,29 +78,8 @@ moreinff(todo){
 
 
 
-   // /////////////////////////////////////////////////////////////////
-  //    this.serve.findByName("userId").subscribe(data=>{this.Workers=data});
-      // this.serve.findByName("אמירן דביר").subscribe(data=>{this.Workers=data});
-     
-      // this.serve.getall().subscribe(data=>{this.Workers=data});//אפשר לשלוח את הנתיב
-
-      // this.serve.getOne(1).subscribe(data =>{this.worker=data[1]});
-     
-        
-   
-    
-      
-      
-     
-      
-      
-     
-       
-        // this.user={"userId":''}
-        
-
+ 
       
    
       
-      // פתיחת קומפוננטה של מידע נוסף 
-  
+     
