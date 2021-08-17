ec1157387c3b62c792170832fa9fb8693ee488b6
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 18:02:26 +0300

    Update new-user.component.ts

--- angular/src/app/components/new-user/new-user.component.ts
+++ angular/src/app/components/new-user/new-user.component.ts
@@ -1,64 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
-// import { PortalHost } from '@angular/cdk/portal';
-// import { PhotographerService } from '../../services/Sphotographer/photographer.service';
-// import { CustomerService } from '../../services/Scustomers/customer.service';
-// import { Router, ActivatedRoute, Params } from '@angular/router';
-// import { customer } from '../../Classes/customer';
 
-
-// @Component({
-//   selector: 'app-new-user',
-//   templateUrl: './new-user.component.html',
-//   styleUrls: ['./new-user.component.css']
-// })
-// export class NewUserComponent implements OnInit {
-//   [x: string]: any;
-//   userId: string;
-//   paaswordc:string
-//   customerId: number;
-//   constructor(private router:Router , private serv:CustomerService, private activatedRoute: ActivatedRoute, public customer: customer ) {
-//    //constructor(private serv:CustomerService ,router:Router) {
-//   // this.user={};
-//   // this.user={"userId":'',"username":"","namchatan":" ","nameCala":" ","dateWedding":" ","taktziv":" ","mailChatan":" ","mailCala":" ","misinvited":" "};
-  
-//   }
-//   // serv:CustomerService 
-
-//   ngOnInit() {
-//     this.activatedRoute.params.subscribe((params: Params) => {
-//       if (params['userId'] == '0'){
-//         this.customer = new customer(); 
-//         this.customerId = 0;
-//         this.serv.setCustomer(this.customer);
-//       }
-//       else {
-//       this.customerId =parseInt(params['userId'] ,10);
-//       console.log('customer', this.serv.cust);
-//       this.serv.cust.dateWedding = new Date( this.serv.cust.dateWedding);
-//       this.customer = this.serv.cust;
-//       }
-//     });
-
-//   }
-  
-//   addcustomer(){
-    
-//     if(this.customer.username==this.paaswordc)
-//     {
-//      this.serv.submit(this.customerId, this.customer).subscribe();   
-//     }
-//      else
-//      alert("הסיסמה שגויה נסה שוב ");debugger;
-//     }
-   
-
-
-//   precentt(){
-//     //this.user.isNew=true;
-//          this.serv.UserDetailes =this.user     //את פרטי הוזר שהוכנסUserDetailes הכנסתי לתוך ה
-//         this.router.navigate(['precent',this.customerId]);
-//    }
-//   }
 
 import { PortalHost } from '@angular/cdk/portal';
 import { PhotographerService } from '../../services/Sphotographer/photographer.service';
@@ -122,7 +62,7 @@ export class NewUserComponent implements OnInit {
 
   precentt(){
     //this.user.isNew=true;
-         this.serv.UserDetailes =this.user     //את פרטי הוזר שהוכנסUserDetailes הכנסתי לתוך ה
+         this.serv.UserDetailes =this.user 
         this.router.navigate(['precent',this.customerId]);
    }
 
