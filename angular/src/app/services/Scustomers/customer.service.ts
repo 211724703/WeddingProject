3fa8635f7ab649a00be175f58efce60182ee7315
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-17 18:25:55 +0300

    Update customer.service.ts

--- angular/src/app/services/Scustomers/customer.service.ts
+++ angular/src/app/services/Scustomers/customer.service.ts
@@ -8,21 +8,21 @@ import { customer } from '../../Classes/customer';
 })
 export class CustomerService {
 
-  // public cust:BehaviorSubject<customer>=new BehaviorSubject <customer>(null);
+  
   public cust: customer = null;
-  public UserDetailes: customer; //מכיל פרטי לקוח 
+  public UserDetailes: customer; 
 
   urlServiece = 'http://localhost:57438/api/';
 
 
-  constructor(private http: HttpClient) { ///מקבל את הhttp והוא מעביר את הקריאות שרת
+  constructor(private http: HttpClient) { 
   }
 
 
-  //הוספת לקוח חדש
-  submit(id, BB: any)// פרטים ומכניס לדטה בייס מקבל 
+  
+  submit(id, BB: any)
   {
-    // const httpOptions = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+   
     if (id == '0') {
       return this.http.put(this.urlServiece + "Customers", BB);
     } else {
@@ -32,7 +32,7 @@ export class CustomerService {
 
   }
 
-  //עידכון לקוח קיים
+  
   PutUpdateCustomer(custumer1: customer): Observable<customer> {
     debugger;
 
@@ -47,23 +47,17 @@ export class CustomerService {
     return this.cust;
   }
 
-  //מחיקת לקוח קיים
+  
   DeleteCust(id: string) {
     return this.http.delete(this.urlServiece + "Customers/" + id);
 
   }
 
-  //שליפת שם משתמש וסיסמא
-  findCustomer(id: number, name: string): Observable<any> {//לשנות את הID לSTRING
-    // debugger;
-    //  return this.http.get(this.urlServiece+"Customers/Getcustomer/"+id/+name);
+  
+  findCustomer(id: number, name: string): Observable<any> {
+ 
 
     return this.http.get<any>(this.urlServiece + "Customers/Getcustomer/" + id + "/" + name)
-
-    // .subscribe((data)=>{
-    //   debugger;
-    //   this.cust.next(data);
-    // })
 
   }
 
@@ -77,11 +71,6 @@ export class CustomerService {
 
 
 }
-  // בן
-  // tacziv= ][]
-
-  // this .costomer= cost
-  // ng model=costomer.name
-
+  
 
 
